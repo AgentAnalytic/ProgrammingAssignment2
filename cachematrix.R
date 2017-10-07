efeb087e842d6e51966edc2aeaea5e4927eb50bc
Abhishek Katyal <31210676+AgentAnalytic@users.noreply.github.com>
2017-10-07 21:51:37 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## This function "makeCacheMatrix((x = matrix(), SET = "FALSE")" takes in two arguments
+## where 'x' is the inputed matrix and 'SET' is used for initializing the list that will store the inputed matrix(s)
+## IMPORTANT : when executing this function 'makeCacheMatrix' for the FIRST time ONLY ,
+## the 'SET' argument must be passed as "TRUE"  and after executing it once do not enter the 'SET' argument(let it be default)
+## for eg.
+## x <- matrix(1:4,2,2)
+## makeCacheMatrix(x,"TRUE") // only enter "TRUE" for the first time while executing the function
+## x <- matrix(1:4,2,2)
+## makeCacheMatrix(x)         // do not enter "TRUE" OR "FALSE" after the first execution
+## z <- matrix(rnorm(4),2,2)
+## makeCacheMatrix(z)     
 
+makeCacheMatrix <- function(x = matrix(), SET = "FALSE") {
+  if (SET) {
+    cachedMatrix <<- list(1)
+    cachedInverse <<- list(1)
+    len <<- length(cachedMatrix)
+    
+    inverse <- cacheSolve(x, cachedMatrix, cachedInverse)
+    inverse
+  }
+  else{
+    inverse <- cacheSolve(x, cachedMatrix, cachedInverse)
+    inverse
+  }
+  
+  
+  
+  
+  
+  
 }
 
 
-## Write a short comment describing this function
+## This function "cacheSolve(x, cachedMatrix, cachedInverse)", takes in three agruments
+## where : 'x' is inputed matrix,
+## 'cachedMatrix' is a list of cached matrix that were inputed previously,
+## 'cachedInverse' is the list of the previously cached matrix inputs.
+## This function does a linear search on the cachedMatrix with the inputed matrix 'x'
+## if the search is positive then the cached inverse matrix is returned else
+## the inputed matrix 'x' is inserted at the end of the list 'cachedMatrix' and its inverse
+## at the end of the list 'cachedInverse', finally the computed inverse is returned
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+
+cacheSolve <- function(x, cachedMatrix, cachedInverse) {
+  len <<- length(cachedMatrix)
+  
+  
+  
+  for (i in 1:len) {
+    if (identical(cachedMatrix[[i]], x))
+    {
+      message("getting cached data")
+      
+      return(cachedInverse[[i]])
+      
+      
+      
+    }
+    
+  }
+  
+  cachedMatrix[[(len + 1)]] <<- x
+  cachedInverse[[(len + 1)]] <<- solve(x)
+  return(cachedInverse[[len + 1]])
+  
+  
 }
